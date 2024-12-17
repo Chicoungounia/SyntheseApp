93600c5b2bc2e390f5f8ac560daa397088f1c4bc
Chicoungounia <jeromef7@hotmail.fr>
2024-12-17 22:38:35 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "./footer/footer.component";
+import { HeaderComponent } from "./header/header.component";
+import { User } from './models/User';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'SyntheseApp';
+
+
+
+
+
+
+ngOnInit()
+{
+  let user:User = {
+    id:1,
+    name: "Albert",
+    email: "ouroauzro@gmail.com"
+  }
+  console.log(user);
+  
+}
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { UsersComponent } from './users/users.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  
+    {
+        path:"users",
+        component: UsersComponent,
+        pathMatch: "full"
+    },
+    {
+        path:"about",
+        component: AboutComponent,
+        pathMatch: "full"
+    },
+];
